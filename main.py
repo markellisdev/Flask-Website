d176c425fff0160813ee0ddbaa1431d42e56d3b6
Mark Ellis <markellisdev@gmail.com>
2020-05-22 14:25:11 -0500

    Commit of initial files

---
+++ main.py
@@ -0,0 +1,16 @@
+# helloTest.py
+# at the end point/call method hello, which returns "Hello World"
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/") # at the endpoint /
+def home():
+    return render_template('home.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
